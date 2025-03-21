cecb1e5eedf8a29a09e81003bc7414ff1e7fa981
Pierre Dok <pierredok@outlook.fr>
2025-03-22 00:31:21 +0100

    adding 2 Navbar for better adaptability on the navbar and more easy to manipulate it

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,122 +1,12 @@
 "use client";
+import { usePathname } from "next/navigation";
 
-import { motion } from "framer-motion";
-import Grid from "@/components/ui/grid";
-import Link from "next/link";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Logo from "@/public/image/StanDesjeuxLOGO.png";
+import NavbarDefault from "./NavbarDefault";
+import NavbarHomepage from "./NavbarHomepage";
 
 export default function LayoutNavbar() {
-  const [openMenu, setOpenMenu] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  const [topDivLogo, setTopDivLogo] = useState<number>(64);
-  const [widthDivLogo, setWidthDivLogo] = useState<number>(250);
-  const [logoSize, setLogoSize] = useState<{
-    normal: number;
-    animated: number;
-  } | null>(null);
+  const pathname = usePathname();
+  const isHomepage = pathname === "/";
 
-  useEffect(() => {
-    const screenWidth = window.innerWidth;
-    const mobile = screenWidth <= 1024;
-    const desktop = screenWidth <= 1440;
-    setIsMobile(mobile);
-    setLogoSize(
-      desktop ? { normal: 92, animated: 242 } : { normal: 92, animated: 424 }
-    );
-    setTopDivLogo(desktop ? 64 : 80);
-    setWidthDivLogo(desktop ? 250 : 450);
-  }, []);
-
-  if (!logoSize || !widthDivLogo || !topDivLogo) return null;
-
-  return (
-    <nav className="fixed z-30 top-5 left-0 w-full text-[15px]">
-      <Grid className="gap-[12px]">
-        {/* {isHomepage ? ( */}
-        <div className="relative">
-          <motion.div
-            initial={{ display: "block" }}
-            animate={{ display: "none" }}
-            transition={{ duration: 0.5 }}
-            style={{ top: `${topDivLogo}px`, width: widthDivLogo }}
-            className="top-16 z-10 fixed w-[500px] h-[100px] bg-white"
-          ></motion.div>
-          <motion.div
-            initial={{ y: "100%", opacity: 0 }}
-            animate={{ y: "0%", opacity: 1 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-            className="relative"
-          >
-            <motion.div
-              className="col-start-1 col-span-2 desktop:col-span-1 pt-[4px]"
-              initial={{ width: logoSize.animated }}
-              animate={{ width: logoSize.normal }}
-              transition={{
-                delay: 1,
-                duration: 1,
-                ease: [0.16, 0.93, 0.2, 0.8],
-              }}
-            >
-              <Link href="/">
-                <Image src={Logo} alt="Logo Name" />
-              </Link>
-            </motion.div>
-          </motion.div>
-        </div>
-        {/* ) : (
-          <motion.div
-            className="col-start-1 col-span-2 desktop:col-span-1 pt-[4px]"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.2, duration: 1, ease: "easeInOut" }}
-          >
-            <Link href="/">
-              <Image src={Logo} alt="Logo Name" />
-            </Link>
-          </motion.div>
-        )}*/}
-        <motion.div
-          className="col-start-3 desktop:col-start-2"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 2, duration: 1 }}
-        >
-          <Link href="/about">About</Link>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 2, duration: 1 }}
-          className="col-start-6 tablet:col-start-9 laptop:col-start-12 col-span-2"
-          onMouseEnter={!isMobile ? () => setOpenMenu(true) : undefined}
-          onMouseLeave={!isMobile ? () => setOpenMenu(false) : undefined}
-          onClick={isMobile ? () => setOpenMenu(!openMenu) : undefined}
-        >
-          <p className="pb-5 text-right w-auto">Menu</p>
-          <motion.div
-            initial={{ y: -20, opacity: 0, display: "none" }}
-            animate={
-              openMenu
-                ? { y: 0, opacity: 1, display: "flex" }
-                : { y: -20, opacity: 0, display: "none" }
-            }
-            transition={{ duration: 0.2, ease: "easeInOut" }}
-            className={`flex flex-col text-[#CECECE] items-end gap-2 w-auto`}
-          >
-            <Link href="/stillLife" className="hover:text-black">
-              Still Life
-            </Link>
-            <Link href="/art" className="hover:text-black">
-              Art
-            </Link>
-            <Link href="/films" className="hover:text-black">
-              Films
-            </Link>
-          </motion.div>
-        </motion.div>
-      </Grid>
-    </nav>
-  );
+  return isHomepage ? <NavbarHomepage /> : <NavbarDefault />;
 }
