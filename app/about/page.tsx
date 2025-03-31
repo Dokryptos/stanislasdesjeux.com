f1cf2df6b37d5d73388a7cf9edeb4f2c407280d9
Pierre Dok <pierredok@outlook.fr>
2025-03-31 16:53:55 +0200

    done with about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,71 @@
+"use client";
+import { motion } from "framer-motion";
+import Grid from "@/components/ui/grid";
+
 export default function About() {
-  return <div>about</div>;
+  return (
+    <motion.div className="pt-[78px] text-[15px]" initial={{}}>
+      <Grid className="gap-6">
+        <div className="col-start-1 col-span-5 tablet:col-span-7 laptop:col-start-3 laptop:col-span-6 desktop:col-start-2 desktop:col-span-6">
+          <p>
+            Stanislas Desjeux est un photographe de nature morte vivant et
+            travaillant à Paris. Son activité s’équilibre entre travail
+            éditorial, commande publicitaire, et une recherche artistique et
+            expérimentale plus personnelle. Son écriture solaire et organique
+            invite à une exploration sensorielle du quotidien, dans laquelle
+            nature et objets deviennent les protagonistes d’un jeu de formes et
+            de matières. Sa démarche sonde les limites du tangible et les
+            mystères du quotidien, à la recherche constante d’une brèche dans le
+            réel. 
+          </p>
+        </div>
+        <div className="col-start-1 col-span-6 laptop:col-start-3 flex flex-col desktop:col-start-2 desktop:col-span-6 ">
+          <p>Représenté en France par Marie Valat</p>
+          <div className="flex pb-2">
+            <a
+              href="https://www.marievalat.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              www.marievalat.com
+            </a>
+            <p className="pr-1 pl-1">|</p>
+            <a
+              href="mailto:contact@marievalat.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              contact@marievalat.com{" "}
+            </a>
+          </div>
+          <a href="tel:+33 6 22 80 25 80">+33(0) 6 22 80 25 80</a>
+          <a
+            href="https://www.google.com/maps/place/21+Av.+de+Tourville/@48.8538312,2.3078164,20.5z/data=!4m7!3m6!1s0x47e6702657586e6d:0x9be1a279b72f0148!4b1!8m2!3d48.853826!4d2.3080599!16s%2Fg%2F11bw4c1m5r?hl=fr&entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            21 avenue de Tourville, 75007 Paris
+          </a>
+        </div>
+        <div className="col-start-1 col-span-6 laptop:col-start-9 laptop:col-span-3 flex-col flex laptop:row-start-1">
+          <p className="pb-2">Contact</p>
+          <a
+            href="mailto:stanislas.desjeux@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            stanislas.desjeux@gmail.com
+          </a>
+          <a href="tel:+33 6 06 88 20 20">+33 (0)6 06 88 20 20</a>
+          <a
+            href="https://www.instagram.com/stan.desjeux/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            instagram @stan.desjeux
+          </a>
+        </div>
+      </Grid>
+    </motion.div>
+  );
 }
