c624e270c6cfce1b78b7398a1da3bf22991740dd
Pierre Dok <pierredok@outlook.fr>
2025-04-07 23:01:42 +0200

    check

--- components/art/slug/imageItemDesktop.tsx
+++ components/art/slug/imageItemDesktop.tsx
@@ -28,7 +28,7 @@ const ImageItemDesktop = ({ img, onImageLoad }: ImageItemProps) => {
   });
 
   // Transformation de la progression du scroll en échelle
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.2]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
 
   return (
     <motion.div
