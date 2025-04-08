97a6957d43228acc3175ba97ecd631767428c870
Pierre Dok <pierredok@outlook.fr>
2025-04-08 21:59:48 +0200

    more trigger zone for mobile art slug animation

--- components/art/slug/artMobileGallerySlug.tsx
+++ components/art/slug/artMobileGallerySlug.tsx
@@ -39,7 +39,7 @@ export default function ArtGallerySlug({ artGallery }: artGallerySlug) {
           setImagesGallery((prev) => [...prev, ...newImages]);
         }
       },
-      { threshold: 0.1, rootMargin: "0px 0px 200px 0px" }
+      { threshold: 0.1, rootMargin: "0px 0px 500px 0px" }
     );
 
     if (loadMoreRef.current) {
