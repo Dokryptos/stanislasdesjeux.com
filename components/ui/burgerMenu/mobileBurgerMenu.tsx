cbaf024caf8d1bc3901f4e9730ea34dc0df6c070
Pierre Dok <pierredok@outlook.fr>
2025-04-14 01:17:26 +0200

    animation burger menu

--- components/ui/burgerMenu/mobileBurgerMenu.tsx
+++ components/ui/burgerMenu/mobileBurgerMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 
 export default function MobileBurgerMenu({
@@ -32,37 +32,40 @@ export default function MobileBurgerMenu({
           <p className="pb-5">Menu</p>
         </div>
       )}
-
-      <motion.div
-        initial={{ height: 0, opacity: 0 }}
-        animate={
-          openMenu ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }
-        }
-        transition={{ duration: 0.2, ease: "easeInOut" }}
-        className={`absolute top-9 right-0 tablet:right-5 flex flex-col text-black items-end gap-2  w-dvw bg-white z-20 pr-5 pb-5 pt-5`}
-      >
-        <Link
-          href="/stillLife"
-          className="hover:text-black"
-          onClick={() => setOpenMenu(false)}
+      <AnimatePresence>
+        <motion.div
+          initial={{ height: 0, opacity: 0 }}
+          animate={
+            openMenu
+              ? { height: "auto", opacity: 1 }
+              : { height: 0, opacity: 0 }
+          }
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+          className={`overflow-hidden absolute top-9 right-0 tablet:right-5 flex flex-col text-black items-end gap-2  w-dvw bg-white z-20 pr-5 pb-5 pt-5`}
         >
-          Still Life
-        </Link>
-        <Link
-          href="/art"
-          className="hover:text-black"
-          onClick={() => setOpenMenu(false)}
-        >
-          Art
-        </Link>
-        <Link
-          href="/films"
-          className="hover:text-black"
-          onClick={() => setOpenMenu(false)}
-        >
-          Films
-        </Link>
-      </motion.div>
+          <Link
+            href="/stillLife"
+            className="hover:text-black"
+            onClick={() => setOpenMenu(false)}
+          >
+            Still Life
+          </Link>
+          <Link
+            href="/art"
+            className="hover:text-black"
+            onClick={() => setOpenMenu(false)}
+          >
+            Art
+          </Link>
+          <Link
+            href="/films"
+            className="hover:text-black"
+            onClick={() => setOpenMenu(false)}
+          >
+            Films
+          </Link>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
