28c489953047155e257dcb9db4d247c3c9a602fe
Pierre Dok <pierredok@outlook.fr>
2025-04-18 01:21:10 +0200

    adding css for video

--- components/slug/videoDisplay.tsx
+++ components/slug/videoDisplay.tsx
@@ -10,7 +10,6 @@ export default function VideoDisplay({ video, title }: VideoDisplayProps) {
   return (
     <div className="col-start-1 col-span-6 tablet:col-start-2 tablet:col-span-7 laptop:col-start-4 laptop:col-span-6 flex justify-center items-center w-full h-dvh">
       <MuxPlayer
-        nohotkeys
         autoPlay={true}
         playbackId={video.asset.playbackId}
         key={title}
